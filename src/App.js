4a5b5bd7c898b77e0a6c278e814df8fdece10f30
Shiv Rustagi <shiv@tryinterlock.com>
2023-03-12 04:52:48 -0700

    major overhaul on styling

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const Child = (props) => {
     const data = {
       prompt: prompt,
       model: 'text-curie-001',
-      temperature: 0,
+      temperature: 0.8,
       max_tokens: 400,
     }
     const url = 'https://api.openai.com/v1/completions'
@@ -53,16 +53,14 @@ const Child = (props) => {
   }
 
   return (<div className="llmbox">
-    <h1>LLM Call {props.number + 1}</h1>
+    <h1>LLM Call #{props.number + 1}</h1>
     <form onSubmit={handleSubmit}>
       <label>
         <h2>Prompt:</h2>
-        {/* <textarea name="prompt" value={prompt} onChange={handleChange} /> */}
         <TextareaAutosize className="textarea" name="prompt" value={prompt} onChange={handleChange} />
-        
       </label>
       
-      <button id="prev" onClick={handlePrev}>Insert Most Recent Output</button>
+      <button id="prev" onClick={handlePrev}><em>Append Most Recent Output</em></button>
       <input type="submit" value="Submit" />
     </form>
     <div>
@@ -94,6 +92,11 @@ const App = () => {
   const [numChildren, setNumChildren] = useState(1);
   const [lastResult, setLastResult] = useState("");
   const [API_key , setAPI_key] = useState("");
+  const [instructions, setInstructions] = useState(true);
+
+  const hideInstructions = (event) => {
+    setInstructions(!instructions);
+  }
 
   const handleKeyChange = (event) => {
     setAPI_key(event.target.value);
@@ -118,15 +121,36 @@ const App = () => {
 
   return (
     <div>
-    <div className="api_key">
-      <label>
-        <h2>OpenAI API Key:</h2>
-        <input name="apikey" value={API_key} onChange={handleKeyChange} />
-      </label>
-    </div>
-    <Parent addFn={addLlmBox} remFn={remLlmBox} >
-      {children}
-    </Parent>
+      <div id="contact">
+        <a className="linkbox" href="#"><img className="icon" src="gh.png" />GitHub</a>
+        <a className="linkbox" href="mailto:me+promptlab@shivan.sh"><img className="icon" src="us.png" />Contact Us</a>
+      </div>
+      <div className="bodyWrapper">
+        <div className="api_key">
+          <div id="instructions">
+            <h2 onClick={hideInstructions}>Instructions (click/tap to hide)</h2>
+            <div id="instructions-content" style={{display: instructions ? "block" : "none"}}>
+              <p>To use this tool:</p>
+              <ol>
+                <li>Get an API key from <em><a href="https://platform.openai.com/">OpenAI</a></em></li>
+                <li>Enter your API key in the box below</li>
+                <li>Fill out your first prompt in LLM Call #1</li>
+                <li>Click submit to get the result from <code>text-curie-001 (temperature: 0.8, max_tokens: 400, everything else default)</code></li>
+                <li>Note that the result of LLM Call #1 is now stored; click "Add LLM Call"</li>
+                <li>Write a new prompt in LLM Call #2, inserting the results of your previous call wherever relevant with the button.</li>
+                <li>Iterate as necessary, adding and removing LLM calls as required!</li>
+              </ol>
+            </div>
+          </div>
+          <label>
+            <h2>OpenAI API Key:</h2>
+            <input name="apikey" value={API_key} onChange={handleKeyChange} />
+          </label>
+        </div>
+        <Parent addFn={addLlmBox} remFn={remLlmBox} >
+          {children}
+        </Parent>
+      </div>
     </div>
   );
 }
